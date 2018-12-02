69c9097311a172c7051a0ca49bb1a8877ae277e5
Holevirus <tivy1996@gmail.com>
2018-12-02 00:28:34 -0600

    js

--- WebContent/script.js
+++ WebContent/script.js
@@ -26,70 +26,13 @@ window.onload = function(){
 	});
 	//
 	//
-	//
-	// Get the modal
-	var modal2 = document.getElementById('myModal2');
-	
-	// Get the button that opens the modal
-	var btn2 = document.getElementById("myBtn2");
-	
-	// Get the <span> element that closes the modal
-	var span2 = document.getElementsByClassName("close2")[0];
-	
-	// When the user clicks the button, open the modal 
-	btn2.onclick = function() {
-	    modal2.style.display = "block";
-	}
-	
-	// When the user clicks on <span> (x), close the modal
-	span2.onclick = function() {
-	    modal2.style.display = "none";
-	}
-	
-	// When the user clicks anywhere outside of the modal, close it
-	// When the user clicks anywhere outside of the modal, close it
-	window.addEventListener('click', function(event) {
-		if (event.target == modal2) {
-	        modal2.style.display = "none";}
-	});
-	//
-	//
-	
-	
+
 	
 	
 	//
 	//
 	//
-	//
-	// Get the modal
-	var modal3 = document.getElementById('myModal3');
 	
-	// Get the button that opens the modal
-	var btn3 = document.getElementById("myBtn3");
-	
-	// Get the <span> element that closes the modal
-	var span3 = document.getElementsByClassName("close3")[0];
-	
-	// When the user clicks the button, open the modal 
-	btn3.onclick = function() {
-	    modal3.style.display = "block";
-	}
-	
-	// When the user clicks on <span> (x), close the modal
-	span3.onclick = function() {
-	    modal3.style.display = "none";
-	}
-	
-	// When the user clicks anywhere outside of the modal, close it
-	// When the user clicks anywhere outside of the modal, close it
-	window.addEventListener('click', function(event) {
-		if (event.target == modal3) {
-	        modal3.style.display = "none";}
-	});
-	//
-	//
-	//
 	//
 	//
 	// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
@@ -140,18 +83,7 @@ window.onload = function(){
 				b5.style.display = "block";
 			}
 			
-			window.addEventListener('click', function(event) {
-				if (event.target == box1) {
-			        b1.style.display = "block";}
-			});
-			window.addEventListener('click', function(event) {
-				if (event.target == box2) {
-			        b2.style.display = "block";}
-			});
-			window.addEventListener('click', function(event) {
-				if (event.target == box3) {
-			        b3.style.display = "block";}
-			});
+			
 	
 	
 	
